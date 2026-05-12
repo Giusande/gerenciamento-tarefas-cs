b42dcfbe97ec6ff26910fcffb93d8b7c098a0918
Giusande <giulianno0013@gmail.com>
2026-05-11 23:39:30 -0300

    atualização

---
+++ src/main/java/com/backend/task_ger/TaskGerApplication.java
@@ -0,0 +1,13 @@
+package com.backend.task_ger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskGerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskGerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/task_ger/TaskGerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.task_ger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskGerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
