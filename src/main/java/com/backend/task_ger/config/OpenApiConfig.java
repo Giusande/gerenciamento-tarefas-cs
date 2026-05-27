6c674cbb6b2575e1ad42c073ab9b51e760033844
Giusande <giulianno0013@gmail.com>
2026-05-27 14:15:57 -0300

    teste completos  27-05-26

---
+++ src/main/java/com/backend/task_ger/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.backend.task_ger.config;
+
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info().title("TaskGer API").version("1.0").description("API para gerenciamento de tarefas"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
